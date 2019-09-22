c5ba8df048ce7af8a7dd96940072efca2cd23a03
Матвей Мурашко <stranger2626@gmail.com>
2019-09-23 00:02:15 +0300

    refactore code slightly

--- test/e2e/step_definitions/actionSteps/browserInteractions.js
+++ test/e2e/step_definitions/actionSteps/browserInteractions.js
@@ -1,5 +1,5 @@
 "use strict";
-let {Then, When, Given} = require('cucumber');
+let { When } = require('cucumber');
 const highlightElement = require('../utils/stepFunctions.js').highlightElement;
 const logger = require('../../config/loggerConfig.js').logger;
 
